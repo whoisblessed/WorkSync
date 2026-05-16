7a5e1e3ecd7f3871c5b035094040e722b62ea956
crtmachiny <crtmachiny@tf.otbebucb>
2026-05-16 23:39:50 +0300

    Фикс БД

--- app/models/employee_event.py
+++ app/models/employee_event.py
@@ -1,14 +1,13 @@
 from sqlalchemy import ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.orm import Mapped, mapped_column
 
 from app.db import Base
 
 
 class EmployeeEvent(Base):
     __tablename__ = "employee_events"
-    
+
     id: Mapped[int] = mapped_column(primary_key=True)
-    
+
     employee_id: Mapped[int] = mapped_column(ForeignKey("employees.id"))
     event_id: Mapped[int] = mapped_column(ForeignKey("events.id"))
-    
--- app/models/event.py
+++ app/models/event.py
@@ -1,18 +1,34 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from enum import Enum
 from datetime import datetime
 
-from sqlalchemy import String
+from sqlalchemy import String, DateTime
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.db import Base
 
 
+if TYPE_CHECKING:
+    from app.models import Employee
+
+
+class EventType(Enum):
+    task = "task"
+    meeting = "meeting"
+
+
 class Event(Base):
     __tablename__ = "events"
-    
+
     id: Mapped[int] = mapped_column(primary_key=True)
+    type: Mapped[EventType] = mapped_column()
     title: Mapped[str] = mapped_column(String(150))
     description: Mapped[str] = mapped_column(String(500))
-    start_at: Mapped[datetime] = mapped_column()
-    end_at: Mapped[datetime] = mapped_column()
+    start_at: Mapped[datetime] = mapped_column(DateTime(timezone=True))
+    end_at: Mapped[datetime] = mapped_column(DateTime(timezone=True))
 
-    employees: Mapped[list["Employee"]] = relationship(secondary="employee_events", back_populates="events")
+    employees: Mapped[list[Employee]] = relationship(
+        secondary="employee_events", back_populates="events"
+    )
